d4e966fa19dc768a54b9ecf52eb87ccf2bd94092
Yacine Benmansour <yacinebenmansourpro@gmail.com>
2025-03-13 00:55:37 +0100

    basic setup, shadcn ui, theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
+
 export default function page() {
   return (
-    <div>
-      <h1 className="text-3xl text-blue-600">Hello to multivendor</h1>
+    <div className="p-5">
+      <div className="w-full flex justify-end">
+        <ThemeToggle />
+      </div>
+      <h1 className="text-3xl text-blue-600 font-inter">
+        Hello to multivendor
+      </h1>
+      <Button>Click me</Button>
     </div>
   );
 }
